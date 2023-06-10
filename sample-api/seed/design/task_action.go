4c364eb8350565a1f2520a046f6d096becc98962
podhmo <ababjam61+github@gmail.com>
2023-06-10 21:05:04 +0900

    chore: pagination

--- sample-api/seed/design/task_action.go
+++ sample-api/seed/design/task_action.go
@@ -1,6 +1,21 @@
 package design
 
-import "github.com/podhmo/gos/openapigen"
+import (
+	"fmt"
+
+	"github.com/podhmo/gos/openapigen"
+)
+
+// helper function for create Pagination[T]
+func pagination(b *openapigen.Builder, typ openapigen.Type) *openapigen.Object {
+	return b.Object(
+		b.Field("total", b.Int()),
+		b.Field("more", b.Bool()),
+		b.Field("cursor", b.String()).Required(false),
+		b.Field("data", typ),
+	).
+		Title(fmt.Sprintf("Pagination[%s]", typ))
+}
 
 var (
 	Task = openapigen.Define("Task", b.Object(
@@ -13,6 +28,6 @@ var (
 var (
 	ListTask = b.Action("ListTask",
 		b.Input(b.Param("sort", b.String().Enum([]string{"createdAt", "-createdAt"})).AsQuery()),
-		b.Output(b.Array(Task)),
+		b.Output(pagination(b, b.Array(Task))),
 	)
 )
